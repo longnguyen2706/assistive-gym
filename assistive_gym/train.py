f41dad99895b4bbc12dfa8fd7ce1e05a54f51ce2
Louis Nguyen <ndlong95@gmail.com>
2023-06-15 15:20:27 -0400

    remove target points that have collision. also train with energy

--- assistive_gym/train.py
+++ assistive_gym/train.py
@@ -28,12 +28,20 @@ def inverse_dynamic(human):
     return ivd
 
 
-def draw_point(point, size=0.01, color=[1, 0, 0, 1]):
-    sphere = p.createVisualShape(p.GEOM_SPHERE, radius=size, rgbaColor=color)
-    multiBody = p.createMultiBody(baseMass=0,
-                                  baseVisualShapeIndex=sphere,
+def create_point(point, size=0.01, color=[1, 0, 0, 1]):
+    sphere = p.createCollisionShape(p.GEOM_SPHERE, radius=size)
+    id = p.createMultiBody(baseMass=0,
+                                  baseCollisionShapeIndex=sphere,
                                   basePosition=np.array(point))
     p.setGravity(0, 0, 0)
+    return id
+
+
+def draw_point(point, size=0.01, color=[1, 0, 0, 1]):
+    sphere = p.createVisualShape(p.GEOM_SPHERE, radius=size, rgbaColor=color)
+    p.createMultiBody(baseMass=0,
+                      baseVisualShapeIndex=sphere,
+                      basePosition=np.array(point))
 
 
 def make_env(env_name, smpl_file, coop=False, seed=1001):
@@ -66,10 +74,10 @@ def get_link_positions(human):
     return link_pos
 
 def cal_energy_change(env, original_link_positions, current_link_positions):
-    g = -9.81  # gravitational acceleration
+    g = 9.81  # gravitational acceleration
     human_id = env.human.body
-    total_energy_change = 0
-
+    potential_energy_initial = 0
+    potential_energy_final = 0
     # Get the number of joints
     num_joints = p.getNumJoints(human_id)
 
@@ -87,40 +95,36 @@ def cal_energy_change(env, original_link_positions, current_link_positions):
             # velocity = p.getLinkState(human_id, i, computeLinkVelocity=1)[6:8]
             mass = p.getDynamicsInfo(human_id, i)[0]
         # Calculate initial potential energy
-        potential_energy_initial = mass * g * original_link_positions[i][2] # z axis
-        potential_energy_final = mass * g * current_link_positions[i][2]
+        potential_energy_initial += mass * g * original_link_positions[i][2] # z axis
+        potential_energy_final += mass * g * current_link_positions[i][2]
         # Add changes to the total energy change
-        total_energy_change += potential_energy_final - potential_energy_initial
-    print(f"Total energy change: {total_energy_change}")
+    total_energy_change = potential_energy_final - potential_energy_initial
+    print(f"Total energy change: {total_energy_change}", f"Potential energy initial: {potential_energy_initial}", f"Potential energy final: {potential_energy_final}")
 
-    return total_energy_change
+    return total_energy_change, potential_energy_initial, potential_energy_final
 
-def cost_fn(env, solution, target_pos, end_effector="right_hand", is_self_collision = False, is_env_collision= False,  energy_change = 0):
+def cost_fn(env, solution, target_pos, end_effector="right_hand", is_self_collision = False, is_env_collision= False,  energy_final = 0):
     human = env.human
 
     real_pos = p.getLinkState(human.body, human.human_dict.get_dammy_joint_id(end_effector))[0]
     dist = eulidean_distance(real_pos, target_pos)
     m = human.cal_chain_manipulibility(solution, end_effector)
 
-    cost = dist + 1.0/m + np.abs(energy_change)/10.0
+    # cost = dist + 1.0/m + np.abs(energy_final)/1000.0
+    cost = 1.0/m + energy_final/100
     if is_self_collision:
-        cost+=10
+        cost+=100
     if is_env_collision:
-        cost+=10
+        cost+=100
     print("euclidean distance: ", dist, "manipubility: ", m, "cost: ", cost)
 
     return cost, m, dist
 
 def generate_target_points(env, num_points):
-    # init points
-    # human_pos = p.getBasePositionAndOrientation(env.human.body, env.human.id)[0]
-    # points = uniform_sample(human_pos, 0.5, 20)
     human = env.human
     right_hand_pos = p.getLinkState(human.body, human.human_dict.get_dammy_joint_id("right_hand"))[0]
-    # points = uniform_sample(right_hand_pos, 0.5, num_points)
-    points = fibonacci_evenly_sampling_range_sphere(right_hand_pos, [0.25, 0.5], 50)
-    # points = fibonacci_sphere(right_hand_pos, 0.5, num_points)
-    return points
+    points = fibonacci_evenly_sampling_range_sphere(right_hand_pos, [0.25, 0.5], 100)
+    return get_valid_points(env, points)
 
 
 def get_initial_guess(env, target=None):
@@ -158,7 +162,7 @@ def plot(vals, title, xlabel, ylabel):
     plt.show()
 
 
-def plot_CMAES_metrics(mean_cost, mean_dist, mean_m):
+def plot_CMAES_metrics(mean_cost, mean_dist, mean_m, mean_energy):
     # Plot the fitness values
     plot(mean_cost, "Cost Function", "Iteration", "Cost")
 
@@ -167,6 +171,9 @@ def plot_CMAES_metrics(mean_cost, mean_dist, mean_m):
 
     # Plot the manipubility values
     plot(mean_m, "Manipubility Values", "Iteration", "Manipubility")
+
+    # Plot energy
+    plot (mean_energy, "Energy Values", "Iteration", "Energy")
 
 def test_collision():
     # print("collision1: ", human.check_self_collision())
@@ -218,15 +225,36 @@ def cal_cost(env, solution, target,  original_self_collisions, original_collisio
     print("self collision: ", is_self_collision, "env collision: ", env_collisions, "is env collision: ",
           is_env_collision)
 
-    energy_change = cal_energy_change(env, original_link_positions, cur_link_positions)
+    energy_change, energy_original, energy_final = cal_energy_change(env, original_link_positions, cur_link_positions)
     cost, m, dist = cost_fn(env, solution, target, is_self_collision=is_self_collision, is_env_collision=is_env_collision,
-                            energy_change=energy_change)
-    return cost, m, dist, energy_change
+                            energy_final=energy_final)
+    return cost, m, dist, energy_final
 
 
 def get_save_dir(save_dir, env_name, smpl_file):
     smpl_name = smpl_file.split('/')[-1].split('.')[0]
     return os.path.join(save_dir, env_name, smpl_name)
+
+
+def get_valid_points(env, points):
+    point_ids = []
+    for point in points:
+        id = create_point(point, size=0.01)
+        point_ids.append(id)
+    p.performCollisionDetection(physicsClientId=env.id)
+
+    valid_points = []
+    valid_ids = []
+    for idx, point in enumerate(points):
+        id = point_ids[idx]
+        contact_points = p.getContactPoints(bodyA=id, physicsClientId=env.id)
+        if len(contact_points)==0:
+            valid_points.append(point)
+            # valid_ids.append(id)
+        # else:
+        #     p.removeBody(id)
+        p.removeBody(id)
+    return valid_points, valid_ids
 
 
 # args.env, args.seed, args.num_points, args.smpl_file, args.save_dir, args.render_train
@@ -242,7 +270,7 @@ def train(env_name, seed=0,  num_points = 50, smpl_file = 'examples/data/smpl_bp
     save_dir = get_save_dir(save_dir, env_name, smpl_file)
     os.makedirs(save_dir, exist_ok=True)
 
-    points = generate_target_points(env, num_points)
+    points, point_ids= generate_target_points(env, num_points)
     pickle.dump(points, open(os.path.join(save_dir, "points.pkl"), "wb"))
 
     actions = []
@@ -251,8 +279,12 @@ def train(env_name, seed=0,  num_points = 50, smpl_file = 'examples/data/smpl_bp
     cost = 0
     env_object_ids= [env.robot.body, env.furniture.body, env.plane.body] # set env object for collision check
     human = env.human
+    points = [0]
     for (idx, target) in enumerate(points):
-        draw_point(target, size=0.01)
+        # draw_point(target, size=0.01)
+        # point_id= point_ids[idx]
+        # p.changeVisualShape(point_id, -1, rgbaColor=[0, 1, 0, 1])
+        draw_point(target, size=0.01, color=[0, 1, 0, 1])
         # original value
         original_joint_angles = human.get_joint_angles(human.controllable_joint_indices)
         original_link_positions = get_link_positions(human)
@@ -260,9 +292,9 @@ def train(env_name, seed=0,  num_points = 50, smpl_file = 'examples/data/smpl_bp
         original_collisions = human.check_env_collision(env_object_ids)
 
         x0 = get_initial_guess(env, None)
-        optimizer = init_optimizer(x0, sigma=0.1)
+        optimizer = init_optimizer(x0, sigma=0.5)
         timestep = 0
-        mean_cost, mean_dist, mean_m, mean_evolution  =[], [], [], []
+        mean_cost, mean_dist, mean_m, mean_energy, mean_evolution  =[], [], [], [], []
 
         while not optimizer.stop():
             timestep += 1
@@ -286,6 +318,7 @@ def train(env_name, seed=0,  num_points = 50, smpl_file = 'examples/data/smpl_bp
             mean_cost.append(np.mean(fitness_values, axis=0))
             mean_dist.append(np.mean(dists, axis=0))
             mean_m.append(np.mean(manipus, axis=0))
+            mean_energy.append(np.mean(energy_changes, axis=0))
 
         # get the best solution value
         env.human.set_joint_angles(env.human.controllable_joint_indices, optimizer.best.x)
@@ -297,7 +330,7 @@ def train(env_name, seed=0,  num_points = 50, smpl_file = 'examples/data/smpl_bp
             "cost": cost,
             "m": m,
             "dist": dist,
-            "energy_change": energy_change,
+            "mean_energy": mean_energy,
             "target": target,
             "mean_cost": mean_cost,
             "mean_dist": mean_dist,
@@ -351,7 +384,7 @@ def render(env_name, smpl_file, save_dir):
         env.human.set_joint_angles(env.human.controllable_joint_indices, action["solution"])
         time.sleep(0.5)
         if idx == best_idx:
-            plot_CMAES_metrics(action['mean_cost'], action['mean_dist'], action['mean_m'])
+            plot_CMAES_metrics(action['mean_cost'], action['mean_dist'], action['mean_m'], action['mean_energy'])
             plot_mean_evolution(action['mean_evolution'])
     # for i in range (1000):
     #     p.stepSimulation(env.id)
