7a9acb01a3f51c779f3d4328fd0250ffd185138e
sammutiti <samanthahmutiti@gmail.com>
2023-08-18 16:59:39 -0400

    WIP: hand within sight

--- batch_train.py
+++ batch_train.py
@@ -6,8 +6,8 @@ from assistive_gym.train import train
 # PERSON_IDS = ['p001', 'p002', 'p003', 'p004', 'p005']
 # SMPL_FILES = ['s01', 's02', 's03', 's04', 's05']
 # SMPL_FILES = ['s06', 's07', 's08', 's09', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19']
-PERSON_IDS = ['p001', 'p002', 'p003', 'p004', 'p005']
-SMPL_FILES = ['s01', 's02', 's03', 's04', 's05', 's06', 's07', 's08', 's09', 's10', 's11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20']
+PERSON_IDS = ['p002']
+SMPL_FILES = ['s01', 's02']
 OBJECTS = ['cane', 'cup', 'pill']
 
 #### Define static configs ####
@@ -15,13 +15,13 @@ SMPL_DIR = 'examples/data/slp3d/'
 ENV = 'HumanComfort-v1'
 SEED = 1001
 SAVE_DIR = 'trained_models'
-RENDER_GUI = False
+RENDER_GUI = True
 SIMULATE_COLLISION = False
 ROBOT_IK = True
 END_EFFECTOR = 'right_hand'
 
 ### DEFINE MULTIPROCESS SETTING ###
-NUM_WORKERS = 20
+NUM_WORKERS = 1
 
 def get_dynamic_configs():
     configs =[]
