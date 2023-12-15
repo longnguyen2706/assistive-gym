add calculation for success metrics on the result. fix issue related to robot ik

assistive_gym/envs/utils/train_utils.py
@@ -4,7 +4,6 @@ import os
 import pickle
 from datetime import datetime
 from typing import Set, Optional
-from assistive_gym.envs.utils.debug_utils import timing
 import gc
 
 import gym
@@ -16,9 +15,9 @@ from cmaes import CMA
 from deprecation import deprecated
 from torch.utils.hipify.hipify_python import bcolors
 
-from assistive_gym.envs.utils.dto import HandoverObject, HandoverObjectConfig, MaximumHumanDynamics, HumanInfo
+from assistive_gym.envs.utils.dto import HandoverObject, HandoverObjectConfig, MaximumHumanDynamics, HumanInfo, \
+    NumpyEncoder
 from assistive_gym.envs.utils.log_utils import get_logger
-from assistive_gym.envs.utils.plot_utils import plot_cmaes_metrics, plot_mean_evolution
 from assistive_gym.envs.utils.point_utils import fibonacci_evenly_sampling_range_sphere, eulidean_distance
 from experimental.urdf_name_resolver import get_urdf_filepath, get_urdf_folderpath
 
@@ -120,11 +119,11 @@ def find_robot_ik_solution(env, end_effector: str, handover_obj: str, init_robot
     """
 
     human, robot, furniture, tool = env.human, env.robot, env.furniture, env.tool
-    if not init_robot_setting:
-        robot_base_pos, robot_base_orient, side = find_robot_start_pos_orient(env, end_effector)
-    else:
-        robot_base_pos, robot_base_orient, side = init_robot_setting.base_pos, init_robot_setting.base_orient, init_robot_setting.robot_side
-
+    # if not init_robot_setting:
+    #     robot_base_pos, robot_base_orient, side = find_robot_start_pos_orient(env, end_effector)
+    # else:
+    #     robot_base_pos, robot_base_orient, side = init_robot_setting.base_pos, init_robot_setting.base_orient, init_robot_setting.robot_side
+    robot_base_pos, robot_base_orient, side = find_robot_start_pos_orient(env, end_effector)
     ee_pos, target_pos = find_ee_ik_goal(human, end_effector, handover_obj)
     # p.addUserDebugLine(ee_pos, target_pos, [1, 0, 0], 5, 0.1)
 
@@ -132,7 +131,7 @@ def find_robot_ik_solution(env, end_effector: str, handover_obj: str, init_robot
                                                                                    [(target_pos, None)],
                                                                                    [(target_pos, None)], human,
                                                                                    base_euler_orient=robot_base_orient,
-                                                                                   attempts=5,
+                                                                                   attempts=10,
                                                                                    random_position=0.3,
                                                                                    max_ik_iterations=50,
                                                                                    collision_objects={
@@ -209,7 +208,7 @@ def find_robot_start_pos_orient(env, end_effector="right_hand", initial_side=Non
         #     side = eyeline_side
         # print ("robot side: ", side, eyeline_side)
         side = "right" if ee_pos[0] > bed_pos[0] else "left"
-        print ("robot side: ", side, " end effector: ", end_effector)
+        # print ("robot side: ", side, " end effector: ", end_effector)
         bed_xx, bed_yy, bed_zz = bed_bb[1] if side == "right" else bed_bb[0]
 
         # find robot base and bb
@@ -1084,14 +1083,3 @@ def translate_bed_to_realworld(env, cord):
     return np.array(cord) - corner
 
 
-class NumpyEncoder(json.JSONEncoder):
-    """ Special json encoder for numpy types """
-
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)

assistive_gym/train.py
@@ -1,22 +1,10 @@
-import pickle
-import time
-
-import argparse
 import traceback
 
-from assistive_gym.envs.utils.dto import RobotSetting
-from assistive_gym.envs.utils.log_utils import get_logger
-from assistive_gym.envs.utils.point_utils import eulidean_distance
-from assistive_gym.envs.utils.train_utils import *
-
 import argparse
-import json
-import multiprocessing
-import os
 import time
+
 from assistive_gym.envs.utils.dto import RobotSetting, InitRobotSetting, EnvConfig, SearchConfig, MainEnvInitResult, \
-    SearchResult, MainEnvProcessInitTask, MainEnvProcessTask, MainEnvProcessTaskType, MainEnvProcessRenderTask, \
-    MainEnvProcessGetHumanRobotInfoTask, TrialResult, HandoverValidity, BestKinematicResult, MeanKinematicResult
+    SearchResult, TrialResult, HandoverValidity, BestKinematicResult, MeanKinematicResult, PosTransPos, HumanRobotResult
 from assistive_gym.envs.utils.train_utils import *
 
 
@@ -42,7 +30,7 @@ def render_robot(env, robot_setting):
     env.tool.reset_pos_orient()
 
 def check_validity(handover_validity: HandoverValidity):
-    if len(handover_validity.new_env_penetrations) or len(handover_validity.new_self_penetrations):
+    if len(handover_validity.new_env_penetrations) or len(handover_validity.new_self_penetrations) or handover_validity.robot_dist_to_target>0.1:
         return False
     return True
 
@@ -53,6 +41,22 @@ def get_human_robot_info(env, result: TrialResult, env_config: EnvConfig):
     human.set_joint_angles(human.controllable_joint_indices, result.joint_angles)
     render_robot(env, robot_setting)
     _, ik_target_pos = find_ee_ik_goal(human, end_effector, handover_obj)
+
+    # return HumanRobotResult(
+    #     pelvis=human.get_pos_orient(human.human_dict.get_fixed_joint_id("pelvis"), center_of_mass=True),
+    #     joint_angles=result.joint_angles,
+    #     ee=PosTransPos(human.get_ee_pos_orient(end_effector), translate_wrt_human_pelvis(human, np.array(
+    #         human.get_ee_pos_orient(end_effector)[0]), np.array(
+    #         human.get_ee_pos_orient(end_effector)[
+    #             1]))),
+    #     ik_target=PosTransPos([np.array(ik_target_pos), np.array(robot_setting.gripper_orient)],
+    #                           translate_wrt_human_pelvis(human, np.array(ik_target_pos),
+    #                                                      np.array(robot_setting.gripper_orient))),
+    #     robot=PosTransPos([np.array(robot_setting.base_pos), np.array(robot_setting.base_orient)],
+    #                       translate_wrt_human_pelvis(human, np.array(robot_setting.base_pos),
+    #                                                  np.array(robot_setting.base_orient))),
+    #     robot_joint_angles=robot_setting.robot_joint_angles)
+
 
     return {
         'pelvis': human.get_pos_orient(human.human_dict.get_fixed_joint_id("pelvis"), center_of_mass=True),
@@ -147,7 +151,6 @@ def run_trial(env, init_result: MainEnvInitResult, search_config: SearchConfig):
         timestep += 1
         solutions = optimizer.ask()
         fitness_values, dists, manipus, energy_changes, torques = [], [], [], [], []
-            
 
         for joint_angles in solutions:
             sr: SearchResult = get_search_result(env, joint_angles, search_config)
@@ -178,7 +181,8 @@ def run_trial(env, init_result: MainEnvInitResult, search_config: SearchConfig):
         if timestep >50 and validity_count / len(solutions) / timestep < 0.25:  # stuck
             LOG.info(
                 f"{bcolors.OKBLUE} Stuck at step: {timestep}, validity count: {validity_count}, validity ratio: {validity_count / len(solutions) / timestep}, {bcolors.ENDC}")
-            break
+            # reinit optimizer
+            optimizer = init_optimizer(x0, 0.05, init_result.joint_lower_limits, init_result.joint_upper_limits)
 
     # # get the kinematic result for best solution
     sr: SearchResult = get_search_result(env, best_angle, search_config)
@@ -264,7 +268,7 @@ def train(env_name, seed=0, smpl_file='examples/data/smpl_bp_ros_smpl_re2.pkl', 
         action = {
             "solution": best_trial_result.joint_angles,
             "cost": best_trial_result.best_kinematic_result.cost,
-            "end_effector": end_effector,
+            "end_effector": env_config.end_effector,
             "m": best_trial_result.best_kinematic_result.m,
             "dist": best_trial_result.best_kinematic_result.dist,
             "energy": best_trial_result.best_kinematic_result.energy,
@@ -315,7 +319,16 @@ def save_train_result(save_dir, env_name, person_id, smpl_file, actions, key, ha
 
     pickle.dump(actions, open(os.path.join(save_dir, "actions.pkl"), "wb"))
 
-    dumped = json.dumps(actions[key]['wrt_pelvis'], cls=NumpyEncoder)
+    json_data = {}
+    action = actions[key]
+
+    
+    for key in ["cost", "end_effector", "m", "dist", "energy", "torque"]:
+        json_data[key] = action[key]
+    json_data["wrt_pelvis"] = json.dumps(action["wrt_pelvis"], cls=NumpyEncoder)
+    json_data["validity"] = action["validity"].to_json()
+    dumped = json.dumps(json_data, indent=4) # dump the whole action dict
+    # dumped = jsonpickle.encode (json_data) # dump the whole action dict
     with open(os.path.join(save_dir, handover_obj + ".json"), "w") as f:
         f.write(dumped)
     f.close()

deepnn/train2.py
@@ -1,7 +1,6 @@
 import json
 import os
 from datetime import datetime
-from timeit import timeit
 
 import numpy as np
 import torch
@@ -13,9 +12,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 from ray.tune.schedulers import ASHAScheduler
 
-from deepnn.utils.misc import timing
+from utils.misc import timing
 from utils.smpl_parser import SMPL_Parser, merge_end_effector_joint_angle
-from deepnn.preprocess.augmented_dataset import AugmentedDataset
+from preprocess.augmented_dataset import AugmentedDataset
 from model.variable_depth_net import VariableDepthNet
 from preprocess.custom_dataset import SMPLDataset
 from utils.data_parser import ModelOutput, ModelOutputHumanOnly
@@ -50,16 +49,16 @@ def get_data_split(batch_size, object):  # 60% train, 20% val, 20% test
     smpl_dataset = SMPLDataset(INPUT_PATH, object, transform=None, human_only = OUTPUT_HUMAN_ONLY)
     augmented_dataset = AugmentedDataset(AUGMENTED_PATH, object, transform=None, human_only = OUTPUT_HUMAN_ONLY)
     # datasets = torch.utils.data.ConcatDataset([smpl_dataset, augmented_dataset])
-    datasets = smpl_dataset
+    datasets = augmented_dataset
     train_size, val_size = int(len(datasets) * 0.8), int(len(datasets) * 0.0)
     test_size = len(datasets) - train_size - val_size
 
     train_dataset, val_dataset, test_dataset = random_split(datasets, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(42))
     print ("train size: ", len(train_dataset), "val size: ", len(val_dataset), "test size: ", len(test_dataset))
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0) # TODO: Change back to true
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8) # TODO: Change back to true
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
     return train_loader, val_loader, test_loader
 
@@ -93,7 +92,6 @@ def train(config, exp_name="ray", is_tune=True):
             # print (features[0][:72])
             # print ("gt_angles: ", gt_angles[0])
 
-            # TODO: move to device here or later
             # Forward pass
             optimizer.zero_grad()
             pred_angles = model(features)
@@ -107,7 +105,7 @@ def train(config, exp_name="ray", is_tune=True):
 
             _, gt_pos = forward_smpl(gt_poses)
             _, pred_pos = forward_smpl(pred_poses)
-            loss = cal_loss(pred_angles.to(device), pred_pos.to(device), gt_angles.to(device), gt_pos.to(device), criterion)
+            loss = cal_loss(pred_angles, pred_pos, gt_angles, gt_pos, criterion)
 
             # Backward pass and optimization
             loss.backward()
@@ -281,7 +279,7 @@ def train_with_ray(config):
     # Run the experiment
     result = tune.run(
         train,
-        resources_per_trial={"cpu": 1},
+        resources_per_trial={"cpu": 1, "gpu": 1},
         config=config,
         num_samples=1,
         scheduler=scheduler,
@@ -344,16 +342,16 @@ if __name__ == '__main__':
     # }
 
     # config = {
-    #     "lr": tune.loguniform(1e-5, 0.25 * 1e-1),
+    #     "lr": tune.loguniform(1e-4, 0.25 * 1e-1),
     #     "weight_decay": tune.loguniform(1e-5, 1e-1),
     #     # "dropout": tune.loguniform(0.5*1e-2, 0.5*1e-1),
     #     # "dropout": tune.loguniform(1e-2, 1e-1),
     #     "dropout": tune.choice([0.025,0.05,0.1]),
     #     "layer_sizes": [
-    #         tune.grid_search(list(range(1024, 8192, 512))),
-    #         tune.grid_search(list(range(512, 4096, 256))), tune.grid_search(list(range(256, 1024, 128))),
-    #                     tune.grid_search(list(range(64, 128, 32))), tune.grid_search(list(range(32, 64, 16)))],
-    #     "batch_size": tune.choice([16]),
+    #         tune.grid_search(list(range(1024, 8192, 1024))),
+    #         tune.grid_search(list(range(512, 4096, 512))), tune.grid_search(list(range(256, 1024, 256))),
+    #                     tune.grid_search(list(range(64, 256, 64))), tune.grid_search(list(range(32, 64, 32)))],
+    #     "batch_size": tune.choice([64]),
     #     "object": "pill"
     # }
     # # # hyper param tuning and train with best config
@@ -362,10 +360,14 @@ if __name__ == '__main__':
     # config: {'lr': 0.0032217072742219115, 'weight_decay': 0.0010331504822697504, 'dropout': 0.025,
     #          'layer_sizes': [1536, 1792, 256, 64, 32], 'batch_size': 16, 'object': 'pill'}
     #train with best config
-    best_config = {'lr': 0.01, 'weight_decay': 4.984018369225781e-05, 'dropout': 0.0,
-                   'layer_sizes': [8192, 2048, 512, 128, 32], 'batch_size': 64, 'object': 'pill'}
-    # # best_config = {'lr': 0.002, 'weight_decay': 1.8385226343464778e-05, 'layer_sizes': [384, 96, 64], 'batch_size': 16, 'dropout': 0.05,  'object': 'pill'}
-    train(best_config,exp_name='t17_aug_2', is_tune=False)
+    # best_config = {'lr': 0.001, 'weight_decay': 4.984018369225781e-05, 'dropout': 0.0,
+    #                'layer_sizes': [8192, 2048, 512, 128, 32], 'batch_size': 64, 'object': 'pill'}
+    # # # best_config = {'lr': 0.002, 'weight_decay': 1.8385226343464778e-05, 'layer_sizes': [384, 96, 64], 'batch_size': 16, 'dropout': 0.05,  'object': 'pill'}
+
+    best_config = {'lr': 0.00034736271280210167, 'weight_decay': 0.012936570242830842, 'dropout': 0.025, 'layer_sizes': [7168, 3584, 256, 128, 32], 'batch_size': 64, 'object': 'pill'}
+
+
+    train(best_config,exp_name='t18_aug_2', is_tune=False)
 
     # load model and output angle to file
     # model_checkpoint= 'model_pill_epoch_200_2023-12-06 22:56:17.022110.ckpt'